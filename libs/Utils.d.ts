a72b0d041f9e455d34deaff74988e2cd1ab76fd9
tianlixuan <35161751+xuantianli@users.noreply.github.com>
2022-09-20 15:01:54 +0800

    Add files via upload

---
+++ libs/Utils.d.ts
@@ -0,0 +1,6 @@
+declare function UtilsEncryptAES(word:string,key:string):string;
+declare function UtilsDecryptAES(word:string,key:string):string;
+declare function UtilsSaveFileToJson(userData:string):void;
+declare function UtilsBase64Encode(word:string):string;
+declare function UtilsBase64Decode(code:string):string;
+declare function UtilsGetFingerprintID():string;
